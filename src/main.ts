d93f183684269720b6271ad2a1ad00c68a6ffc9b
Ruchir <ruchirmat@gmail.com>
2019-08-19 00:39:39 -0700

    Updated for amplify

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,9 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import Amplify from '@aws-amplify/core';
-import awsconfig from './aws-exports';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
-Amplify.configure(awsconfig);
 
 if (environment.production) {
   enableProdMode();
